970fb2b925a76e4600ddbd282567009773ae0a72
Yura <aderevs@gmail.com>
2024-02-18 04:47:14 +0200

    task 2

--- Lab1Charp/Program.cs
+++ Lab1Charp/Program.cs
@@ -1,10 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more 
- static void task1()
+
+//1.14. радіус кола, площа круга якої дорівнює s
+static void Task1()
 {
     Console.WriteLine("Task1 !");
     Console.Write("s = ");
     double s;
-    if(double.TryParse(Console.ReadLine(), out s))
+    if (double.TryParse(Console.ReadLine(), out s))
     {
         Console.WriteLine("r = {0}", Math.Sqrt(s / Math.PI));
     }
@@ -14,6 +16,43 @@
     }
 
 }
+
+//2.14. яка з цифр тризначного числа більше: перша чи друга;
+static void Task2()
+{
+    Console.WriteLine("Task2!");
+    Console.WriteLine("Enter c number");
+    short num;
+    if (short.TryParse(Console.ReadLine(), out num))
+    {
+        if (num >= 100 && num <= 999)
+        {
+            byte firstDigit = (byte)(num / 100);
+            byte secondDigit = (byte)((num - firstDigit * 100) / 10);
+            if (firstDigit > secondDigit)
+            {
+                Console.WriteLine("First digit is bigger");
+            }
+            else if (secondDigit > firstDigit)
+            {
+                Console.WriteLine("Second digit is bigger");
+            }
+            else if (firstDigit == secondDigit)
+            {
+                Console.WriteLine("First and second digits are equal");
+            }
+        }
+        else
+        {
+            Console.WriteLine("number must be 3-values");
+        }
+    }
+    else
+    {
+        Console.WriteLine("invalid input:(");
+    }
+
+}
 Console.WriteLine("Lab 1 !");
-task1();
+Task2();
 // continue ...
